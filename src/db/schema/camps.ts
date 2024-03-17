2ed4a4edf94b11e63ea741396bdfc78b81539ca4
NickP-real <nick_narak@rocketmail.com>
2024-03-18 02:47:05 +0700

    feat(db): add camps schema

---
+++ src/db/schema/camps.ts
@@ -0,0 +1,103 @@
+import {
+	boolean,
+	date,
+	index,
+	integer,
+	numeric,
+	pgTable,
+	serial,
+	text,
+	uniqueIndex,
+	varchar
+} from 'drizzle-orm/pg-core';
+import softDeleteColumns from '../soft-delete-columns';
+import { staff, student } from './users';
+
+export const camp = pgTable(
+	'camp',
+	{
+		id: serial('id').primaryKey(),
+		name: varchar('name', { length: 256 }).notNull(),
+		fromDate: date('from_date').notNull(),
+		toDate: date('to_date').notNull(),
+		description: text('text').notNull(),
+		hasLaundry: boolean('hasLaundry').notNull().default(true),
+		laundryPrice: numeric('laundry_price', { precision: 10, scale: 2 }),
+		...softDeleteColumns
+	},
+	(camp) => ({
+		campIdx1: uniqueIndex('camp_idx_1').on(camp.name),
+		campIdx2: index('camp_idx_2').on(camp.fromDate),
+		campIdx3: index('camp_idx_3').on(camp.toDate),
+		campIdx4: index('camp_idx_4').on(camp.hasLaundry),
+		campIdx5: index('camp_idx_5').on(camp.deletedAt),
+		campIdx6: index('camp_idx_6').on(camp.createdAt),
+		campIdx7: index('camp_idx_7').on(camp.updatedAt)
+	})
+);
+
+export const major = pgTable(
+	'major',
+	{
+		id: serial('id').primaryKey(),
+		name: varchar('name', { length: 256 }).notNull(),
+		...softDeleteColumns
+	},
+	(major) => ({
+		majorIdx1: uniqueIndex('major_idx_1').on(major.name),
+		majorIdx2: index('major_idx_2').on(major.deletedAt),
+		majorIdx3: index('major_idx_3').on(major.createdAt),
+		majorIdx4: index('major_idx_4').on(major.updatedAt)
+	})
+);
+
+export const campMajor = pgTable(
+	'camp_major',
+	{
+		id: serial('id').primaryKey(),
+		campId: integer('camp_id').references(() => camp.id),
+		majorId: integer('major_id').references(() => major.id),
+		...softDeleteColumns
+	},
+	(campMajor) => ({
+		campMajorIdx1: index('camp_major_idx_1').on(campMajor.campId),
+		campMajorIdx2: index('camp_major_idx_2').on(campMajor.majorId),
+		campMajorIdx3: index('camp_major_idx_3').on(campMajor.deletedAt),
+		campMajorIdx4: index('camp_major_idx_4').on(campMajor.createdAt),
+		campMajorIdx5: index('camp_major_idx_5').on(campMajor.updatedAt)
+	})
+);
+
+export const campStaff = pgTable(
+	'camp_staff',
+	{
+		id: serial('id').primaryKey(),
+		campId: integer('camp_id').references(() => camp.id),
+		staffId: integer('staff_id').references(() => staff.id),
+		...softDeleteColumns
+	},
+	(campStaff) => ({
+		campStaffIdx1: index('camp_staff_idx_1').on(campStaff.campId),
+		campStaffIdx2: index('camp_staff_idx_2').on(campStaff.staffId),
+		campStaffIdx3: index('camp_staff_idx_3').on(campStaff.deletedAt),
+		campStaffIdx4: index('camp_staff_idx_4').on(campStaff.createdAt),
+		campStaffIdx5: index('camp_staff_idx_5').on(campStaff.updatedAt)
+	})
+);
+
+export const campStudent = pgTable(
+	'camp_student',
+	{
+		id: serial('id').primaryKey(),
+		campId: integer('camp_id').references(() => camp.id),
+		studentId: integer('staff_id').references(() => student.id),
+		...softDeleteColumns
+	},
+	(campStudent) => ({
+		campStudentIdx1: index('camp_student_idx_1').on(campStudent.campId),
+		campStudentIdx2: index('camp_student_idx_2').on(campStudent.studentId),
+		campStudentIdx3: index('camp_student_idx_3').on(campStudent.deletedAt),
+		campStudentIdx4: index('camp_student_idx_4').on(campStudent.createdAt),
+		campStudentIdx5: index('camp_student_idx_5').on(campStudent.updatedAt)
+	})
+);
