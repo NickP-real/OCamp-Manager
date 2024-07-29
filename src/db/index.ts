24bee01e3eb72340959e67e50d32ac735f2f237f
nickp-real <nick_narak@rocketmail.com>
2024-07-30 00:53:56 +0700

    test(build): changing env variable

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,16 @@
 import { sql } from '@vercel/postgres';
 import { drizzle as prodDrizzle, type VercelPgDatabase } from 'drizzle-orm/vercel-postgres';
 import pg from 'pg';
+import { NODE_ENV } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { drizzle as localDrizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 export let db: NodePgDatabase | VercelPgDatabase;
 
 // local pg
-if (process.env.NODE_ENV === 'development') {
+if (NODE_ENV === 'development') {
 	const { Client } = pg;
-	const client = new Client({ connectionString: process.env.DATABASE_URL });
+	const client = new Client({ connectionString: env.DATABASE_URL });
 	await client.connect();
 	db = localDrizzle(client, { logger: true });
 } else {
