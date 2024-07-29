d60165d2bde9596e55051293f0216586020e4804
nickp-real <nick_narak@rocketmail.com>
2024-07-30 00:46:32 +0700

    fix(build): attemp 3

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,14 @@
 import { sql } from '@vercel/postgres';
 import { drizzle as prodDrizzle, type VercelPgDatabase } from 'drizzle-orm/vercel-postgres';
-import { NODE_ENV, DATABASE_URL } from '$env/static/private';
 import pg from 'pg';
 import { drizzle as localDrizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 export let db: NodePgDatabase | VercelPgDatabase;
 
 // local pg
-if (NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
 	const { Client } = pg;
-	const client = new Client({ connectionString: DATABASE_URL });
+	const client = new Client({ connectionString: process.env.DATABASE_URL });
 	await client.connect();
 	db = localDrizzle(client, { logger: true });
 } else {
