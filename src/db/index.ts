b5d1628004e7196cabb9c7cab4725cd9853f7183
nickp-real <nick_narak@rocketmail.com>
2024-07-30 00:28:58 +0700

    fix(db): local development with pg
    vercel cli somehow port error so I switch to local db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,14 @@
-import { sql } from '@vercel/postgres';
-import { drizzle } from 'drizzle-orm/vercel-postgres';
+// import { sql } from '@vercel/postgres';
+// import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { env } from '$env/dynamic/private';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import pg from 'pg';
 
-export const db = drizzle(sql, { logger: true });
+// local pg
+const { Client } = pg;
+const client = new Client({ connectionString: env.DATABASE_URL });
+await client.connect();
+export const db = drizzle(client, { logger: true });
+
+// vercel
+// export const db = drizzle(sql, { logger: true });
